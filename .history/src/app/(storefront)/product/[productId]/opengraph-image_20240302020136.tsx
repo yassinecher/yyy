28df59cbf7e2b9fa73f2ab488c52124ddbfd3993
yassinecher <yassinecherni9@gmail.com>
2024-03-02 02:19:12 +0100

    up

--- .history/src/app/(storefront)/product/[productId]/opengraph-image_20240302020136.tsx
+++ .history/src/app/(storefront)/product/[productId]/opengraph-image_20240302020136.tsx
@@ -31,7 +31,7 @@ export default async function og({ params }: Props) {
        
         <div  tw=" flex flex-col w-[500px]  ">
             <p tw="text-fuchsia-200 text-4xl flex font-black m-5">{prod?.name}</p>
-            <p tw="text-fuchsia-200 text-md flex font-black m-5">{prod?.description}</p>
+        
         </div>
         <div tw="w-[150px]  flex items-start absolute ml-3  top-0 ">
         <img tw="w-[150px]" src="https://gaminggear.tn/images/logo%20(3).png" alt={prod?.name}  /> 
